528f7f8d64b0499ee14052534473fed1f536a49c
AshutoshD2011 <ashudahe11@gmail.com>
2024-05-03 10:36:19 +0530

    new

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const jsonServer = require("json-server"); // importing json-server library
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
@@ -6,5 +7,7 @@ const port = process.env.PORT || 3000; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
+
+console.log(port);
 
 server.listen(port);
